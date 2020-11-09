59a67be1ac1db9776eec5a962ff0da7773cc64f7
Shrena Patel <shrena.p@hotmail.co.uk>
2020-11-09 14:52:38 +1300

    snake case to camel case sanitisation

--- server/routes/candidates.js
+++ server/routes/candidates.js
@@ -5,8 +5,20 @@ const db = require('../db/candidates')
 
 router.get('/', (request, response) => {
     db.getCandidates()
-    .then(candidate => {
-        response.json(candidate)
+    .then((candidates) => {
+        const sanitised = candidates.map((candidate) => {
+            return {
+                candidateId: candidate.id,
+                locationSeeking: candidate.location_seeking,
+                currentRole: candidate.current_role,
+                roleSeeking: candidate.role_seeking,
+                workRights: candidate.work_rights,
+                yearsExp: candidate.years_exp,
+                location: candidate.location,
+                qualifications: candidate.qualifications
+            }
+        })
+        return response.json(sanitised)
     })
     .catch(err => {
         console.log(err)

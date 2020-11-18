77ea43475d7352bf00a1efa136cca64849c4cf0b
Shrena Patel <shrena.p@hotmail.co.uk>
2020-11-18 16:07:18 +1300

    employer db and route functions set up

--- server/routes/employers.js
+++ server/routes/employers.js
@@ -5,8 +5,19 @@ const db = require('../db/employers')
 
 router.get('/', (request, response) => {
     db.getEmployers()
-    .then(employer => {
-        response.json(employer)
+    .then((employers) => {
+        const sanitised = employers.map((employer) => {
+            return {
+                
+                companyId: employer.id,
+                companyName: employer.company_name,
+                companySize: employer.company_size,
+                companyExpertise: employer.company_expertise,
+                companyLocation: employer.company_location,
+                
+            }
+        })
+        return response.json(sanitised)
     })
     .catch(err => {
         console.log(err)
